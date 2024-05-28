69182d2136b6b1c61cfcb5fe512d68ae18118e99
Bon <Blackball007b@gmail.com>
2024-05-28 07:55:51 +0700

    add upload_to_raw_bucket task

--- dags/e_commerce.py
+++ dags/e_commerce.py
@@ -2,54 +2,67 @@ import datetime
 
 from airflow.decorators import dag, task, task_group
 from airflow.operators.python import PythonOperator
-from great_expectations_provider.operators.great_expectations import GreatExpectationsOperator
-from great_expectations.core.batch import BatchRequest
-from great_expectations.data_context.types.base import (
-    DataContextConfig,
-    CheckpointConfig
-)
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 from fake_data_generator.generate_data import (generate_customer_data,
                                                generate_product_data,
                                                generate_order_data)
 
 
+MY_GX_DATA_CONTEXT = "include/great_expectations"
+
+
 @dag(
-    start_date=datetime.datetime(2024, 1, 1),
+    start_date=datetime.datetime(2024, 8, 1),
     schedule="@daily",
     default_args={"retries": 1}
 )
 def e_commerce():
     @task_group(group_id='generate_data')
     def generate_data():
-        create_data = PythonOperator(
+        customer_data = PythonOperator(
             task_id="generate_customer_data",
             python_callable=generate_customer_data,
             op_kwargs={"num_rows": 1000}
         )
-        create_data2 = PythonOperator(
+        product_data = PythonOperator(
             task_id="generate_product_data",
             python_callable=generate_product_data,
             op_kwargs={"num_rows": 1000}
         )
-        create_data3 = PythonOperator(
+        order_data = PythonOperator(
             task_id="generate_order_data",
             python_callable=generate_order_data,
             op_kwargs={"num_rows": 1000}
         )
 
-    @task_group(group_id='data_validator')
-    def validate_data():
-        ge_operator = GreatExpectationsOperator(
-            task_id="ge_operator",
-            checkpoint_name="my_checkpoint",
-            batch_request=BatchRequest(
-                datasource_name="my_datasource",
-                data_connector_name="default_inferred_data_connector_name",
-                data_asset_name="my_data_asset",
-            ),
+    @task_group(group_id='upload_to_raw_bucket')
+    def upload_to_raw_bucket():
+        upload_customer_data = LocalFilesystemToS3Operator(
+            task_id="upload_customer_data",
+            filename="/opt/airflow/data/customer_data.csv",
+            dest_key="customer_data.csv",
+            dest_bucket="bon-raw-data-17835146",
+            aws_conn_id="aws_default"
+        )
+        upload_product_data = LocalFilesystemToS3Operator(
+            task_id="upload_product_data",
+            filename="/opt/airflow/data/product_data.csv",
+            dest_key="product_data.csv",
+            dest_bucket="bon-raw-data-17835146",
+            aws_conn_id="aws_default"
+        )
+        upload_order_data = LocalFilesystemToS3Operator(
+            task_id="upload_order_data",
+            filename="/opt/airflow/data/order_data.csv",
+            dest_key="order_data.csv",
+            dest_bucket="bon-raw-data-17835146",
+            aws_conn_id="aws_default"
         )
 
-    generate_data()
+
+
+
+    generate_data() >> upload_to_raw_bucket()
 
 
 dag = e_commerce()
