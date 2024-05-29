70875f72cf9e98aa593c2bc3ebd6727600c80c1c
Bon <Blackball007b@gmail.com>
2024-05-29 14:48:02 +0700

    implement first data processing task group

--- dags/e_commerce.py
+++ dags/e_commerce.py
@@ -1,19 +1,20 @@
 import datetime
+import zipfile
 import os
 
 from dotenv import load_dotenv
-from airflow.decorators import dag, task
+from airflow.decorators import dag, task, task_group
 from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 
 load_dotenv()
 MY_GX_DATA_CONTEXT = "include/great_expectations"
 
-RAW_BUCKET_NAME = os.getenv('RAW_BUCKET_NAME')
-TRANSFROMED_BUCKET_NAME = os.getenv('TRANSFROMED_BUCKET_NAME')
-SERVING_BUCKET_NAME = os.getenv('SERVING_BUCKET_NAME')
-
 KAGGLE_USERNAME = os.getenv('KAGGLE_USERNAME')
 KAGGLE_KEY = os.getenv('KAGGLE_KEY')
+
+zip_path = "/opt/airflow/data/ecommerce-purchase-history-from-electronics-store.zip"
+csv_path = "/opt/airflow/data/kz.csv"
+data_dest_path = "/opt/airflow/data/"
 
 
 @dag(
@@ -40,7 +41,38 @@ def e_commerce():
         replace=True
     )
 
-    download_ecommerce_data() >> upload_to_s3
+    @task_group(group_id="first_data_processing")
+    def first_data_processing():
+
+        @task(task_id="unzip_data")
+        def unzip_data(zip_path, dest_path):
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(dest_path)
+
+        unzip_data = unzip_data(zip_path, data_dest_path)
+
+        @task(task_id="remove_zip_file")
+        def remove_zip_file(zip_path):
+            os.remove(zip_path)
+
+        remove_zip_file = remove_zip_file(zip_path=zip_path)
+
+        @task.bash(task_id="rename_file")
+        def rename_file(file_path, csv_file):
+            return f"mv {file_path} {csv_file}"
+
+        rename_file = rename_file(
+            file_path="/opt/airflow/data/kz.csv",
+            csv_file="/opt/airflow/data/ecommerce-electronics-purchase-history.csv"
+        )
+
+        return unzip_data >> remove_zip_file >> rename_file
+
+
+    download_data = download_ecommerce_data()
+    first_processing = first_data_processing()
+
+    download_data >> upload_to_s3 >> first_processing
 
 
 dag = e_commerce()
