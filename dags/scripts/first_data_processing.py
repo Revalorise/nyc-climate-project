cf3657992fc5ff734eb2612fe1cbcdd165a67229
Bon <Blackball007b@gmail.com>
2024-06-05 12:54:55 +0700

    convert functions into class

--- dags/scripts/first_data_processing.py
+++ dags/scripts/first_data_processing.py
@@ -2,27 +2,29 @@
 import zipfile
 
 
-def unzip_data(source: str, destination: str) -> None:
-    """
-    Unzip the contents of a source file to a destination directory.
-    """
-    with zipfile.ZipFile(source, 'r') as zip_file:
-        zip_file.extractall(destination)
+class DataProcessor:
+    @staticmethod
+    def unzip_data(source: str, destination: str) -> None:
+        """
+        Unzip the contents of a source file to a destination directory.
+        """
+        with zipfile.ZipFile(source, 'r') as zip_file:
+            zip_file.extractall(destination)
 
+    @staticmethod
+    def remove_file(file_path: str) -> None:
+        """
+        Remove a file from the file system.
+        """
+        import os
+        if os.path.exists(file_path):
+            os.remove(file_path)
 
-def remove_file(file_path: str) -> None:
-    """
-    Remove a file from the file system.
-    """
-    import os
-    if os.path.exists(file_path):
-        os.remove(file_path)
-
-
-def rename_file(old_name: str, new_name: str) -> None:
-    """
-    Rename a file in the file system.
-    """
-    import os
-    if os.path.exists(old_name):
-        os.rename(old_name, new_name)
+    @staticmethod
+    def rename_file(old_name: str, new_name: str) -> None:
+        """
+        Rename a file in the file system.
+        """
+        import os
+        if os.path.exists(old_name):
+            os.rename(old_name, new_name)
