323c6cdda228f6d4e7d02558e85066dcbd85cbed
Bon <Blackball007b@gmail.com>
2024-06-06 15:37:37 +0700

    remove unused sql scripts

--- include/sql_scripts/insert_data.sql
+++
@@ -1,4 +0,0 @@
-COPY orders (event_time, order_id, product_id, category_id, category_code, brand, price, user_id)
-FROM '/opt/airflow/data/ecommerce-electronics-purchase-history.csv'
-DELIMITER ','
-CSV HEADER;
